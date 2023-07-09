856f4688615b7436733ce984133a58745ff67d6a
Mak <letsmailmak@gmail.com>
2023-07-09 13:59:28 +0600

    done

--- js/deposit.js
+++ js/deposit.js
@@ -4,11 +4,26 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     if (deposit > 0) {
         const sum = parseFloat(deposit) + parseFloat(deposittotal)
         document.getElementById('deposit').innerText = sum
+        document.getElementById('balance').innerText= parseFloat(deposit)+parseFloat(document.getElementById('balance').innerText)
         document.getElementById('deposit-input').value = ' '
     }
     else{
         alert('Cant be 0 or negative')
     }
-
     console.log(deposit, deposittotal)
+})
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const withdraw=document.getElementById('withdraw-input').value 
+    const withdrawt=document.getElementById('withdraw').innerText
+    if (withdraw > 0) {
+        const sum = parseFloat(withdraw) + parseFloat(withdrawt)
+        document.getElementById('withdraw').innerText = sum
+        document.getElementById('balance').innerText= parseFloat(document.getElementById('balance').innerText)-parseFloat(withdraw)
+        document.getElementById('withdraw-input').value = ' '
+    }
+    else{
+        alert('Cant be 0 or negative')
+    }
+    console.log(withdraw, withdrawt)
 })
